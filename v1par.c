f7afaa2c0395d340b1227c6a7f6e815a6ea30f77
Jacob Vosburgh <jacob_vosburgh@yahoo.com>
2019-10-14 18:33:06 -0400

    copied files to add parallelization

---
+++ v1par.c
@@ -0,0 +1,117 @@
+/*
+ * COMP 633:  sequential implementation for n bodies (pa1a)
+ *            V1:  all-pair interactions
+ *                 direct transcription of spec
+ *
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <omp.h>
+
+#define  NMAX    10000
+
+/*
+ * attributes of a body in 2D
+ */
+struct Body {
+  double x,y;    // position
+  double m;      // mass
+  double vx,vy;  // velocity
+  double ax,ay;  // acceleration
+};
+
+/*
+ * n-body system
+ */
+static struct Body P[NMAX];
+
+double G      = 6.673E-11;
+double DeltaT = 0.001;
+int    nts    = 4;     // number of time steps
+int    n;              // number of bodies
+
+
+
+int main(int argc, char * argv[])
+{
+
+  int h, i, j, k;
+  double t1,t2;
+
+  // how many bodies
+  if (argc > 1) {
+    n = atoi(argv[1]);
+    n = (n > 0 && n <= NMAX) ? n : NMAX;
+  }
+
+  /* 
+   * initial state: place n bodies along diagonal of unit square, 
+   * each with unit mass and zero initial velocity and acceleration
+   */
+  for (i = 0; i < n; i++) {
+    P[i].x = P[i].y = (double) i / (double) n;
+    P[i].m = 1.0;
+    P[i].vx = P[i].vy = 0.0;
+    P[i].ax = P[i].ay = 0.0;
+  }
+
+
+  /*
+   *  n-body simulation for nts steps
+   */
+  t1 = omp_get_wtime();
+  for (k = 1; k <= nts; k++) {
+
+    for (i = 0; i < n; i++) {
+
+      /*  force accumulator for body i
+       */
+      double fx = 0.0;
+      double fy = 0.0;
+
+      /*  interact with all bodies (except self)
+       */
+      for (j = 0 ; j < n;  j++) {
+
+	if (j != i) { 
+	  double rx, ry, d, d3, c;
+	  
+	  /* direct transcription of f_ij 
+	   */
+	  rx = P[j].x - P[i].x;   
+	  ry = P[j].y - P[i].y;
+	  d  = sqrt((rx * rx) + (ry * ry));
+	  d3 = pow(d,3.0);
+	  fx += G * P[i].m * P[j].m * rx / d3;
+	  fy += G * P[i].m * P[j].m * ry / d3;
+	} /* if */
+
+      }/* for j */
+
+      P[i].ax = fx / P[i].m;
+      P[i].ay = fy / P[i].m;
+
+    }/* for i */
+
+    /* update velocities and positions 
+     */
+    for (i = 0; i < n; i++) {
+      P[i].x  += P[i].vx * DeltaT;
+      P[i].y  += P[i].vy * DeltaT;
+      P[i].vx += P[i].ax * DeltaT; 
+      P[i].vy += P[i].ay * DeltaT; 
+    }
+
+  } /* for k */
+
+  t2 = omp_get_wtime();
+
+  /* report result in units of millions of interactions per second 
+   */
+  {
+    double interactions = nts*n*n;
+    double dt = t2 - t1;
+    printf("   %6d    %.5g \n", n, 1E-6 * interactions / dt); 
+  }
+}
---
+++ v3npar.c
@@ -0,0 +1,119 @@
+/*
+ * COMP 633:  sequential implementation for n bodies (pa1a)
+ *            V3:  half-pair interactions
+ *
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <omp.h>
+
+#define  NMAX    50000
+
+
+/*
+ * attributes of a body in 2D
+ */
+struct Body {
+  double x, y;
+  double m;
+  double vx,vy;  // velocity
+  double fx,fy;  // total force (instead of acceleration)
+};
+
+/*
+ * n-body system
+ */
+static struct Body     P[NMAX];
+
+double G      = 6.673E-11;
+double DeltaT = 0.001;
+int    nts    = 4; 
+int    n;
+
+
+int main(int argc, char * argv[])
+{
+
+  int h, i, j, k;
+  double t1,t2;
+
+  // how many bodies
+  if (argc > 1) {
+    n = atoi(argv[1]);
+    n = (n > 0 && n <= NMAX) ? n : NMAX;
+  }
+
+  /* 
+   * initial state: place n bodies along diagonal of a unit square, 
+   * each with unit mass and zero initial velocity and acceleration
+   * and total force
+   */
+  for (i = 0; i < n; i++) {
+    P[i].x = P[i].y = (double) i / (double) n;
+    P[i].m = 1.0;
+    P[i].vx = P[i].vy = 0.0;
+    P[i].fx = P[i].fy = 0.0;
+  }
+
+  /*
+   *  n-body simulation for nts steps
+   */
+  t1 = omp_get_wtime();
+  for (k = 1; k <= nts; k++) {
+
+    for (i = 0; i < n; i++) {
+
+      double Fx = 0.0, Fy = 0.0;
+
+      /* 
+       * half-pairs:  each pairwise interaction is evaluated once 
+       */
+      for (j = i + 1; j < n;  j++) {
+
+	  double rx, ry, d2, d, c;
+	  
+	  /* interaction caluclation
+	   */
+	  rx = P[j].x - P[i].x;   
+	  ry = P[j].y - P[i].y;   
+	  d2 = (rx * rx) + (ry * ry);
+	  c  = P[j].m * P[i].m / (d2 * sqrt(d2));           
+	  Fx += c * rx;              
+	  Fy += c * ry;  
+	  P[j].fx -= c * rx;  // reciprocal interaction
+	  P[j].fy -= c * ry;             
+
+      } /* j */
+
+      P[i].fx +=  Fx;
+      P[i].fy +=  Fy;
+
+    } /* i */
+
+    /* advance velocities and positions 
+     */
+    for (i = 0; i < n; i++) {
+      P[i].x  += P[i].vx * DeltaT;
+      P[i].y  += P[i].vy * DeltaT;
+      P[i].vx += ((G * P[i].fx) / P[i].m) * DeltaT; 
+      P[i].vy += ((G * P[i].fy) / P[i].m) * DeltaT; 
+      P[i].fx = P[i].fy = 0.0;  // reset forces for next time step
+    }
+
+  } /* k */
+
+  t2 = omp_get_wtime();
+  
+  /* report result in units of millions of interactions per second 
+   */
+  {
+    double interactions = nts * (double) n * (double) n;
+    double dt = t2 - t1;
+    printf("   %6d    %.5g \n", n, 1E-6 * interactions / dt); 
+  }
+
+} 
+
+
+
