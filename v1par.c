0c6d5dbcf4f5c1612c8ef82acd7592b40482d379
Jacob Vosburgh <jacob_vosburgh@yahoo.com>
2019-10-14 19:16:16 -0400

    Dynamic 1
    Guided 1 gave 197

--- v1par.c
+++ v1par.c
@@ -67,7 +67,7 @@ int main(int argc, char * argv[])
 t1 = omp_get_wtime();
  #pragma omp parallel shared(P) private(k,i,j)
 	{
-	   #pragma omp for schedule(guided,1)
+	   #pragma omp for schedule(dynamic ,1)
 	  for (k = 1; k <= nts; k++) {
 
 	    for (i = 0; i < n; i++) {
