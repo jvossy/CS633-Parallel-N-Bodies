a8315f23445d92eac5e15003346ee5aa07d4245d
Jacob Vosburgh <jacob_vosburgh@yahoo.com>
2019-10-14 18:20:36 -0400

    corrected prints

--- v1.c
+++ v1.c
@@ -113,7 +113,7 @@ int main(int argc, char * argv[])
     double interactions = nts*n*n;
     double dt = t2 - t1;
     printf("   %6d    %.5g \n", n, 1E-6 * interactions / dt);
-    printf("   %lf, %lf", p[0].x, p[0].y)
-    printf("   %lf, %lf", p[1].x, p[1].y)
+    printf("   %lf, %lf", P[0].x, P[0].y);
+    printf("   %lf, %lf", P[1].x, P[1].y);
   }
 }
