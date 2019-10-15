54281a143ed7444dce6b81f92bb5d785893a5ab7
Victor Murta <victor.murta97@gmail.com>
2019-10-14 20:45:55 -0400

    updated the benchmarker to replace bench.dat

--- benchmarker.sh
+++ benchmarker.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-FILENAME=bench.dat 
+FILENAME=bench.dat
+rm $FILENAME
+gcc v1par.c -fopenmp -Ofast -o v1par
 for i in 10 20 50 100 200 500 1000 2000 5000 10000
 do
-    ./v1 $i >> $FILENAME
+    ./v1par $i >> $FILENAME
     echo >> $FILENAME
 done
