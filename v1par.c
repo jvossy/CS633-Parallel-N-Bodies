6b18933f08eee35fdca795da17530f6ef40003e2
Jacob Vosburgh <jacob_vosburgh@yahoo.com>
2019-10-14 20:27:43 -0400

    static
    dynamic default gave ~950, comparable to before

--- v1par.c
+++ v1par.c
@@ -68,7 +68,7 @@ t1 = omp_get_wtime();
 for (k = 1; k <= nts; k++) {
  #pragma omp parallel shared(P) private(k,i,j)
 	{
-	   #pragma omp for schedule(dynamic)
+	   #pragma omp for schedule(static)
 	    for (i = 0; i < n; i++) {
 
 	      /*  force accumulator for body i
